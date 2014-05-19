89ecb35b26c975c2360718080160a4af184b4f81
BaptisteHamain <baptistehamain@icloud.com>
2014-05-19 15:20:43 +0200

    first pump on views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
 private
 
   def item_params
-    @item_params = params.require(:item).permit(:price, :picture_url, :description, :title, :location)
+    @item_params = params.require(:item).permit(:price, :pictures_url, :description, :title, :location)
   end
 
   def set_item
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'items#index', as: 'items'
+  root 'items#index'
   resources :items
 
   # The priority is based upon order of creation: first created -> highest priority.
