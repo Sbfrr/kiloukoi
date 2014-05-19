44b46405155da3554c2b1716c498774cd5a221fc
Vincent Porquet <vincent_porquet@outlook.com>
2014-05-19 13:19:03 +0200

    items controller OK

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,52 @@
 class ItemsController < ApplicationController
-  def new
-  end
+ before_action :find_all_items
 
-  def create
-  end
-
-  def edit
-  end
+ before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @items = Item.all
   end
 
   def show
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.create(item_params)
+    redirect_to item_path(item)
+  end
+
+  def find_all_items
+    @items_list = Item.all
+  end
+
+  def edit
+  end
+
   def update
+    @item.update!(item_params)
+
+    #flash [:info] = "Tu viens de mettre à jour ton annonce #{@item.title}"
+
+    redirect_to item_path(@item)
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
   end
+
+private
+
+  def item_params
+    @item_params = params.require(:item).permit(:price, :picture_url, :description, :title, :location)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
