961b5ded21f13f4a1e1f13c96d05609806e4c6c9
Vincent Porquet <vincent_porquet@outlook.com>
2014-05-20 20:19:55 +0200

    user login add and sign up created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :lastname) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :lastname, :birthdate, :email) }
   end
 end
