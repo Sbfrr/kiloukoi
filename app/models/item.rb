1a35e3fbc1edd4e55978da635ee3202f7bf20ccb
sebastien <sebastien@ferre.io>
2014-05-20 10:40:30 +0200

    clean up models to build user model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,3 @@
 class Item < ActiveRecord::Base
-  has_attached_file :picture,
-  styles: { medium: "300x300>", thumb: "100x100>" }
-
-  validates_attachment_content_type :picture,
-  :content_type => /\Aimage\/.*\Z/
 
 end
