d14374b645149fa4e7cc76eabca9914774e4cea7
sebastien <sebastien@ferre.io>
2014-05-21 09:37:01 +0200

    bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140520160000) do
 
   # These are extensions that must be enabled in order to support this database
@@ -24,6 +23,28 @@ ActiveRecord::Schema.define(version: 20140520160000) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "pictures", force: true do |t|
+    t.integer  "item_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+  end
+
+  add_index "pictures", ["item_id"], name: "index_pictures_on_item_id", using: :btree
+
+  create_table "uploads", force: true do |t|
+    t.integer  "item_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: true do |t|
