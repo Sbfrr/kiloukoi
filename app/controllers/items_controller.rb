39ab4e3e6543e7a833c6f69e6c74630154f45acd
sebastien <sebastien@ferre.io>
2014-05-21 09:54:16 +0200

    new branch

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
  before_action :set_item, only: [:show, :edit, :update, :destroy]
-
  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
  respond_to :js, :html
 
