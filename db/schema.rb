5204739ba019f8f084b56c12f720c8af74242bdc
Vincent Porquet <vincent_porquet@outlook.com>
2014-05-21 11:08:53 +0200

    password do run and migration to change name column OK

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520160000) do
+ActiveRecord::Schema.define(version: 20140521110200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,16 +37,6 @@ ActiveRecord::Schema.define(version: 20140520160000) do
 
   add_index "pictures", ["item_id"], name: "index_pictures_on_item_id", using: :btree
 
-  create_table "uploads", force: true do |t|
-    t.integer  "item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -60,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140520160000) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "firstname"
     t.string   "lastname"
     t.datetime "birthdate"
     t.text     "address"
