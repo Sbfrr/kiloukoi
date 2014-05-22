5b0b95a964648be8209b45b7d1ebd1757f8f04c5
Vincent Porquet <vincent_porquet@outlook.com>
2014-05-22 10:19:39 +0200

    implement profile show and edit features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @users = User.all
-  end
+  before_action :authenticate_user!
 
   def show
-  end
-
-  def new
-  end
-
-  def create
   end
 
   def edit
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
+    current_user.update(user_params)
+    redirect_to profile_path
   end
 
-  def destroy
-    @user.destroy
-    redirect_to users_path
-  end
-
-private
-
+  private
   def user_params
-    @user_params = params.require(:user).permit(:email, :firstname, :lastname, :birthdate, :address, :zipcode, :city)
-  end
-
-  def set_user
-    @user = User.find(params[:id])
+    params.require(:user).permit(:firstname, :lastname, :address, :zipcode, :city, :birthdate)
   end
 
 end
