f939359bba48a35677044e3cecf4491d94b5ee5a
sebastien <sebastien@ferre.io>
2014-05-22 11:31:47 +0200

    Booking migration updated and ready to run

--- db/migrate/20140522085804_create_bookings.rb
+++ db/migrate/20140522085804_create_bookings.rb
@@ -1,7 +1,11 @@
 class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
-
+      t.belongs_to :user
+      t.belongs_to :item
+      t.date :arrival
+      t.date :departure
+      t.integer :price
       t.timestamps
     end
   end
